82472cd93c5ab90302fa24e52ca4ef1ef543c1de
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-02 14:27:41 -0700

    add test for potion, armor, attack, level up
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -2,12 +2,12 @@ import { Game, Character } from './../src/rpg.js';
 
 describe('Game', () => {
   let game;
-  let brycen;
-  let michael;
+  let brycen =  ('brycen') ;
+  let michael =  ('michael') ;
+
   beforeEach(() => {
     game = new Game();
   })
-
   test('should create a game with with 0 players', () => {
     expect(game.currentId).toEqual(0);
   });
@@ -15,25 +15,62 @@ describe('Game', () => {
     game.addCharacter(brycen);
     game.addCharacter(michael);
     expect(game.characters).toHaveLength(2);
+  })
+  test('will assign increase current id', () => {
+    game.assignId(brycen);
+    game.assignId(michael);
+    expect(game.currentId).toEqual(2);
   })  
 });
 
+
 describe('Character', () => {
+    let michael;
     let brycen;
     let bane;
     let character;
+    let character2;
+    let UltraPotion = 15;
+    let dragonArmor = 40;
+    let ultimateWeapon = 50;
+
     beforeEach(() => {
       character = new Character(brycen, bane);
+      character2 = new Character(michael, bane)
     })
 
   test('should create a character', () => {
     expect(character.name).toBe(brycen);
     expect(character.playerClass).toBe(bane);
     expect(character.health).toEqual(20);
-    expect(character.level).toEqual(0);
-    expect(character.armor).toBeFalsy();
+    expect(character.level).toEqual(1);
+    expect(character.armor).toStrictEqual(false);
     expect(character.attack).toEqual(5);
-    expect(character.weapon).toBeFalsy();
+    expect(character.weapon).toStrictEqual(false);
     expect(character.inventory).toHaveLength(0);
   });
+  test('should subtract 5 from health', () => {
+    character.attackMove(character2);
+    expect(character2.health).toEqual(15);
+  })
+  test('should add potion value to health', () => {
+    character.usePotion(UltraPotion);
+    expect(character.health).toEqual(35);
+  })
+  test("should add armor value to health and turn armor value to true", () => {
+    character.equipArmor(dragonArmor);
+    expect(character.health).toEqual(60);
+    expect(character.armor).toStrictEqual(true);
+  })
+  test('should add weapon value to attack and weapon value to true', () => {
+    character.equipWeapon(ultimateWeapon);
+    expect(character.attack).toEqual(55);
+    expect(character.weapon).toStrictEqual(true);
+  })
+  test("should add health, attack, and level", () => {
+   character.levelUp();
+   expect(character.health).toEqual(25);
+   expect(character.attack).toEqual(6);
+   expect(character.level).toEqual(2); 
+  })
 });
--- src/rpg.js
+++ src/rpg.js
@@ -4,22 +4,52 @@ export function Game() {
 }
 
 Game.prototype.addCharacter = function(character) {
-  this.characters.push(character)
+  this.characters.push(character);
+  //character.id = this.assignId();
 }
 
-
+Game.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
 
 export function Character(name, playerClass) {
   this.name = name;
   this.playerClass = playerClass; 
   this.health = 20;
-  this.level = 0;
+  this.level = 1;
   this.armor = false;
   this.attack = 5;
   this.weapon = false;
   this.inventory = [];
+  }
+
+Character.prototype.attackMove = function(attackedPlayer) {
+  attackedPlayer.health -= this.attack;
+  return;
+} 
+
+Character.prototype.usePotion = function(potionType) {
+  this.health += potionType;
+  return;
 }
 
+Character.prototype.equipArmor = function(armorType) {
+  this.armor = true;
+  this.health += armorType;
+  return;
+}
 
+Character.prototype.equipWeapon = function(weaponType){
+  this.weapon = true;
+  this.attack += weaponType;
+  return;
+}
+
+Character.prototype.levelUp = function(){
+  this.level += 1;
+  this.health += 5;
+  this.attack += 1;
+}
 
 
