f42fd7cc2645e41bd92e148548b81347d00bf4b7
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-02 10:55:22 -0700

    test character constructor

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -1,8 +1,24 @@
-import { Game } from './../src/rpg.js';
+import { Game, Character } from './../src/rpg.js';
 
 describe('Game', () => {
   test('should create a game with with 0 players', () => {
     const game = new Game();
     expect(game.currentId).toEqual(0);
+  });  
+});
+
+describe('Character', () => {
+  test('should create a character', () => {
+    let brycen;
+    let bane;
+    const character = new Character(brycen, bane);
+    expect(character.name).toBe(brycen);
+    expect(character.playerClass).toBe(bane);
+    expect(character.health).toEqual(20);
+    expect(character.level).toEqual(0);
+    expect(character.armor).toBeFalsy();
+    expect(character.attack).toEqual(5);
+    expect(character.weapon).toBeFalsy();
+    expect(character.inventory).toHaveLength(0);
   });
 });
--- src/rpg.js
+++ src/rpg.js
@@ -2,3 +2,18 @@ export function Game() {
   this.characters = [];
   this.currentId = 0;
 }
+
+
+
+export function Character(name, playerClass) {
+  this.name = name;
+  this.playerClass = playerClass; 
+  this.health = 20;
+  this.level = 0;
+  this.armor = false;
+  this.attack = 5;
+  this.weapon = false;
+  this.inventory = [];
+}
+
+
